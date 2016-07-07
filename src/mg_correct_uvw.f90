310fb5d26bad6c588cf16f0a80427d406ea10320
ngrigri <nicolas.grima@univ-brest.fr>
2016-07-07 11:13:18 +0200

    Removing memory allocation/deallocation

--- src/mg_correct_uvw.f90
+++ src/mg_correct_uvw.f90
@@ -21,9 +21,10 @@ contains
 
     real(kind=rp), dimension(:,:)  , pointer :: dx,dy
     real(kind=rp), dimension(:,:,:), pointer :: zr,zw
-    real(kind=rp), dimension(:,:)  , pointer :: dxu,dyv
-    real(kind=rp), dimension(:,:,:), pointer :: dz,dzw
     real(kind=rp), dimension(:,:,:), pointer :: p
+    real(kind=rp) :: dxu,dyv
+    real(kind=rp) :: dzw
+
 
     !NG comment: constants in a mg_cst.f90 file ?
     real(kind=rp), parameter :: two  = 2._rp
@@ -43,40 +44,17 @@ contains
 
     if (myrank==0) write(*,*)'- correct u,v,w:'
 
-    !! Cell heights
-    allocate(dzw(nz+1,0:ny+1,0:nx+1))
-    do i = 0,nx+1
-       do j = 0,ny+1
-          dzw(1,j,i) = zr(1,j,i)-zw(1,j,i) !!
-          do k = 2,nz
-             dzw(k,j,i) = zr(k,j,i)-zr(k-1,j,i) !!  cell height at w-points
-          enddo
-          dzw(nz+1,j,i) = zw(nz+1,j,i)-zr(nz,j,i) !!
-       enddo
-    enddo
-
-    !! Cell widths
-    allocate(dxu(0:ny+1,nx+1))
-    do i = 1,nx+1
-       do j = 0,ny+1
-          dxu(j,i) = hlf * (dx(j,i)+dx(j,i-1))
-       enddo
-    enddo
-
-    allocate(dyv(ny+1,0:nx+1))
-    do i = 0,nx+1
-       do j = 1,ny+1
-          dyv(j,i) = hlf * (dy(j,i)+dy(j-1,i))
-       enddo
-    enddo
-
     !! Correct
     p => grid(1)%p
 
     do i = 1,nx+1
        do j = 0,ny+1 
           do k = 1,nz
-             u(i,j,k) = u(i,j,k) - one / dxu(j,i)*(p(k,j,i)-p(k,j,i-1))
+
+             dxu = hlf * (dx(j,i)+dx(j,i-1))
+
+             u(i,j,k) = u(i,j,k) - one / dxu * (p(k,j,i)-p(k,j,i-1))
+
           enddo
        enddo
     enddo
@@ -84,24 +62,29 @@ contains
    do i = 0,nx+1
        do j = 1,ny+1 
           do k = 1,nz
-             v(i,j,k) = v(i,j,k) - one / dyv(j,i)*(p(k,j,i)-p(k,j-1,i  ))
+
+             dyv = hlf * (dy(j,i)+dy(j-1,i))
+
+             v(i,j,k) = v(i,j,k) - one / dyv * (p(k,j,i)-p(k,j-1,i  ))
+
           enddo
        enddo
     enddo
 
     do i = 0,nx+1
        do j = 0,ny+1
+
           do k = 2,nz !interior and upper levels
-             w(i,j,k-1) = w(i,j,k-1) - one / dzw(k,j,i)*(p(k,j,i)-p(k-1,j,i))
+             dzw = zr(k,j,i)-zr(k-1,j,i)
+             w(i,j,k-1) = w(i,j,k-1) - one / dzw * (p(k,j,i)-p(k-1,j,i))
           enddo
+
           k = nz+1 !surface
-          w(i,j,k-1) = w(i,j,k-1) - one / dzw(k,j,i)*(-p(k-1,j,i))
+          dzw = zw(nz+1,j,i)-zr(nz,j,i)
+          w(i,j,k-1) = w(i,j,k-1) - one / dzw * (-p(k-1,j,i))
+
        enddo
     enddo
-
-    deallocate(dzw)
-    deallocate(dxu)
-    deallocate(dyv)
 
   end subroutine correct_uvw
 
